254187ac72d50658debbdcc030bdd8ae7f7fecd0
Branislav Vidojevic <branislav.vidojevic@msg-global.com>
2016-11-15 11:23:38 +0100

    finished hello world example

---
+++ final/src/main/java/com/example/firstStep/HelloWorldResource.java
@@ -0,0 +1,16 @@
+package com.example.firstStep;
+
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+/**
+ * Resource which has only one representation.
+ *
+ */
+public class HelloWorldResource extends ServerResource {
+
+	@Get
+	public String represent() {
+		return "hello, world (from the cloud!)";
+	}
+}
